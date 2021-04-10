5ff3acf73cadfb1ed9c039bb4c29667c952c44fb
Andrew Thomson <athomsonguy@gmail.com>
2021-04-10 01:28:37 -0700

    add react

--- src/symbolica/core.cljs
+++ src/symbolica/core.cljs
@@ -1,6 +1,8 @@
-(ns symbolica.core)
+(ns symbolica.core
+  (:require react-dom))
 
 (println "Hello world!")
 
-(defn average [a b]
-  (/ (+ a b) 2.0))
+(.render js/ReactDOM
+         (.createElement js/React "h2" nil "Hello, React!")
+         (.getElementById js/document "app"))
