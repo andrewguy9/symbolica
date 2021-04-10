ea535ffafdd30d9051ed3a8df4d8177793bc8b39
Andrew Thomson <athomsonguy@gmail.com>
2021-04-10 01:48:42 -0700

    add closure code example

--- src/symbolica/core.cljs
+++ src/symbolica/core.cljs
@@ -1,8 +1,18 @@
 (ns symbolica.core
-  (:require react-dom))
+  (:require react-dom
+            [goog.dom :as dom]
+            [goog.dom.classes :as classes]
+            [goog.events :as events])
+  (:import [goog Timer]))
 
-(println "Hello world!")
+(println "Hello ClojureScript React!")
 
 (.render js/ReactDOM
          (.createElement js/React "h2" nil "Hello, React!")
          (.getElementById js/document "app"))
+
+(let [element (dom/createDom "div" "some-class" "Hello Google Closure")]
+  (classes/enable element "another-class" true)
+  (-> (dom/getDocument)
+    .-body
+    (dom/appendChild element)))
