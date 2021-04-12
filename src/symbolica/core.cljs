914568397f1c7d913adabc76096cb1237452d3a0
Andrew Thomson <athomsonguy@gmail.com>
2021-04-12 12:23:49 -0700

    ad reagent instaparse component example of a calculator.

--- src/symbolica/core.cljs
+++ src/symbolica/core.cljs
@@ -5,6 +5,11 @@
             [goog.events :as events]
             [yq]
             [calc]
+            [instaparse.core :as insta]
+            [clojure.pprint :as pp]
+            [clojure.core]
+            [reagent.core :as r]
+            [reagent.dom :as rd]
             )
   (:import [goog Timer]))
 
@@ -25,3 +30,30 @@
 
 (replace_div "js-library" (str "1+2 == " (js/add 1 2)))
 
+(def math
+  (insta/parser "
+                expr = add-sub
+                <add-sub> = mul-div | add | sub
+                add = add-sub <'+'> mul-div
+                sub = add-sub <'-'> mul-div
+                <mul-div> = term | mul | div
+                mul = mul-div <'*'> term
+                div = mul-div <'/'> term
+                <term> = number | <'('> add-sub <')'>
+                number = #'[0-9]+'
+                "))
+
+(defn math_eval [ast]
+  (->> ast
+     (insta/transform
+       {:add +, :sub -, :mul *, :div /, :number cljs.tools.reader/read-string :expr identity})))
+
+(let [text "1-2/(3-4)+5*6"
+      ast (math text)
+      result (math_eval ast)]
+  (rd/render [:div
+              [:p text]
+              [:p (with-out-str (pp/pprint ast))]
+              [:p result]
+              ]
+             (js/document.getElementById "parse-test")))
